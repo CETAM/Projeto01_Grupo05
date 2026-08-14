a87176c66c925e3cd02299ad84ed86fe3f7f06aa
Natanael Borges <natanaelborges2004@gmail.com>
2026-08-14 15:53:49 -0400

    feat: model Usuario

--- src/main/java/cetam/projeto01grupo05/model/Usuario.java
+++ src/main/java/cetam/projeto01grupo05/model/Usuario.java
@@ -1,6 +1,92 @@
 package cetam.projeto01grupo05.model;
 
+import cetam.projeto01grupo05.model.enums.StatusUsuario;
+import cetam.projeto01grupo05.model.enums.TipoUsuario;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuario")
 public class Usuario {
-    private string nome;
-    private int idade;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_usuario;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String senha;
+
+    @Enumerated(EnumType.STRING)
+    private TipoUsuario tipoUsuario;
+
+    @Enumerated(EnumType.STRING)
+    private StatusUsuario status = StatusUsuario.ATIVO;
+
+    public Usuario() {}
+
+    public Usuario(Long id_usuario, String nome, String cpf, String email, String senha, TipoUsuario tipoUsuario, StatusUsuario status) {
+        this.id_usuario = id_usuario;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+        this.tipoUsuario = tipoUsuario;
+        this.status = status;
+    }
+
+    public Long getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(Long id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public TipoUsuario getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(TipoUsuario tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public StatusUsuario getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusUsuario status) {
+        this.status = status;
+    }
 }
