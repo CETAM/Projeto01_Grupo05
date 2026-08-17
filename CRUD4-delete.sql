69bd896c2b62efc91609caccc09922479b3e63a8
Dayana <151537481+dayana-nascimento@users.noreply.github.com>
2026-08-17 14:13:23 -0400

    Delete CRUD4-delete.sql

--- CRUD4-delete.sql
+++
@@ -1,9 +0,0 @@
-USE biblioteca_db;
-
--- Deletando um exemplar específico
-DELETE FROM exemplar 
-WHERE id_exemplar = 1;
-
--- Deletando uma editora //só funcionará se não houver livros vinculados a ela
-DELETE FROM editora 
-WHERE id_editora = 1;
