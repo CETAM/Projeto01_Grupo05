bd6c497668f1120af7dc06386f71192909201858
Natanael Borges <natanaelborges2004@gmail.com>
2026-08-26 16:41:33 -0400

    fix: method findbyusuario

--- src/main/java/cetam/projeto01grupo05/repository/ReservaRepository.java
+++ src/main/java/cetam/projeto01grupo05/repository/ReservaRepository.java
@@ -1,6 +1,7 @@
 package cetam.projeto01grupo05.repository;
 
 import cetam.projeto01grupo05.model.Reserva;
+import cetam.projeto01grupo05.model.Usuario;
 import cetam.projeto01grupo05.model.enums.StatusReserva;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,28 +12,26 @@ import java.util.Optional;
 @Repository
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
 
-    // Reservas de um determinado livro por status
     List<Reserva> findByLivroIdLivroAndStatusOrderByDataReservaAsc(
             Long idLivro,
             StatusReserva status
     );
 
-    // Reservas de um determinado usuário por status
     List<Reserva> findByUsuarioIdUsuarioAndStatus(
             Long idUsuario,
             StatusReserva status
     );
 
-    // Verifica se o usuário já possui uma reserva para determinado livro
     boolean existsByUsuarioIdUsuarioAndLivroIdLivroAndStatus(
             Long idUsuario,
             Long idLivro,
             StatusReserva status
     );
 
-    // Primeira reserva da fila de um livro
     Optional<Reserva> findFirstByLivroIdLivroAndStatusOrderByDataReservaAsc(
             Long idLivro,
             StatusReserva status
     );
+
+    List<Reserva> findByUsuario(Usuario usuario);
 }
